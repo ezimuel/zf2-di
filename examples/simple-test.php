d6e3232108a903ee79838bf4ad1a70fecdf62bc6
Ralph Schindler <ralphschindler@yahoo.com>
2011-05-16 10:40:42 -0500

    Edited the simple-test.php for the DI container, which also explains the "wish-list" for a managing DI framework.

--- examples/simple-test.php
+++ examples/simple-test.php
@@ -13,25 +13,30 @@ spl_autoload_register(function($class) {
 /** BOOTSTRAP START **/
 
 
-// for a kernel based diFramework:
-// $diFramework = new Zend\Di\Framework;
-// $di = $diFramework->getDi();
 $di = new Zend\Di\DependencyInjector;
 
-// this should be automatic (via reflection) replace by something like this:
-// $diFramework->addManagedNamespace('My'); //optional: add seed path - path/to/My
+/**
+ * From this point on, this is a valid usage of Matthew's Di Prototype
+ * Places where I think we can reduce the amount of "setup code" are
+ * wrapped inside curly brances { }
+ */
+
+
+/**
+ * This whole section of code would be replace by something like this:
+ * $diFramework = new Zend\Di\Framework();
+ * $diFramework->setDi($di);
+ * $diFramework->setConfiguration('framework-config.ini'); // or xml, or yaml
+ * $diFramework->bootstrap();
+ */ 
 {
+    /**
+     * this should be automatic (via reflection) replace by something like this:
+     * $diFramework->addManagedNamespace('My'); //optional: add seed path - path/to/My
+     */
     $def1 = new Zend\Di\Definition('My\DbAdapter');
     $def1->setParamMap(array('username' => 0, 'password' => 1));
-}
-
-// this should be from a config file
-// $diFramework->setConfigPath('path/to/config.php');
-$def1->setParams(array('password' => 'bar', 'username' => 'foo'));
-
-// this should be automatic (via reflection) replace by something like this:
-// $diFramework->setClassPath('My', 'path/to/My');
-{
+    
     $def2 = new Zend\Di\Definition('My\Mapper');
     $def2->setParamMap(array('dbAdapter' => 0));
     $def2->setParams(array('dbAdapter' => new Zend\Di\Reference('dbAdapter')));
@@ -44,13 +49,23 @@ $def1->setParams(array('password' => 'bar', 'username' => 'foo'));
     $def4->setParamMap(array('mapper' => 0));
     $def4->setParams(array('mapper' => new Zend\Di\Reference('mapper')));
     
+    // back to $def1 becuase it contains values for parameters
+    $def1->setParams(array('password' => 'bar', 'username' => 'foo'));
+    
+    // make sure to pass the definitions into the actual di container
     $di->setDefinition($def1, 'dbAdapter');
     $di->setDefinition($def2, 'mapper');
     $di->setDefinition($def3, 'repositoryA');
     $di->setDefinition($def4, 'repositoryB');
 }
 
-$repoa = $di->newInstance('repositoryA'); // $repoa = new My\RepositoryA(new My\Mapper(new My\DbAdapter('foo', 'bar')));
+
+
+/**
+ * Same as:
+ * $repoa = new My\RepositoryA(new My\Mapper(new My\DbAdapter('foo', 'bar')));
+ */
+$repoa = $di->newInstance('repositoryA'); 
 echo $repoa;
 
 echo PHP_EOL;
@@ -64,35 +79,49 @@ echo PHP_EOL;
 
 
 
-//    // Replaces this procedure:
-//    $dbAdapter = new My\DbAdapter('foo', 'bar');
-//    $mapper = new My\Mapper($dbAdapter);
-//    $repoA = new My\RepositoryA($mapper);
-//    $repoB = new My\RepositoryB($mapper);
-//    
-//    // Replaces this hand drafted class
-//    class My\Service
-//    {
-//        protected $mapper = null;
-//        public function __construct($config)
-//        {
-//            $dbAdapter = new My\DbAdapter($config->username, $config->password);
-//            $this->mapper = new My\Mapper($dbAdapter);
-//        }
-//        public function getRepositoryA()
-//        {
-//            static $repo = null;
-//            if (!$repo) {
-//                $repo = new My\RepositoryA($this->mapper);
-//            }
-//            return $repo;
-//        }
-//        public function getRepositoryB()
-//        {
-//            static $repo = null;
-//            if (!$repo) {
-//                $repo = new My\RepositoryB($this->mapper);
-//            }
-//            return $repo;
-//        }
-//    }
+
+
+
+/*
+    
+    // The above DI managed container reduces the following hand crafted code
+    // that could be repeated in a large number of places.  the DI container
+    // promotes the DRY principle by reducing the amount of usage or "runtime"
+    // code that is needed to spin up a highly dependent object.
+    
+    $dbAdapter = new My\DbAdapter('foo', 'bar');
+    $mapper = new My\Mapper($dbAdapter);
+    $repoA = new My\RepositoryA($mapper);
+    $repoB = new My\RepositoryB($mapper);
+
+    // Replaces this hand drafted class
+    class My\Service
+    {
+        protected $mapper = null;
+        public function __construct($config)
+        {
+            $dbAdapter = new My\DbAdapter($config->username, $config->password);
+            $this->mapper = new My\Mapper($dbAdapter);
+        }
+        public function getRepositoryA()
+        {
+            static $repo = null;
+            if (!$repo) {
+                $repo = new My\RepositoryA($this->mapper);
+            }
+            return $repo;
+        }
+        public function getRepositoryB()
+        {
+            static $repo = null;
+            if (!$repo) {
+                $repo = new My\RepositoryB($this->mapper);
+            }
+            return $repo;
+        }
+    }
+
+*/
+
+
+
